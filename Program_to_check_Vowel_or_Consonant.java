9c759591303c83f8720403db72be0272efffb2b7
Nyshitha9 <85222301+Nyshitha9@users.noreply.github.com>
2022-03-04 00:48:51 +0530

    import java.util.*; class A  {     public static void main(String[] args)  {       Scanner s = new Scanner(System.in); 	     char c = s.next().charAt(0); 	{ 		if (c == 'a' || c == 'e' || c == 'i' || c== 'o' || c == 'u'|| c == 'A'|| c == 'E'|| c == 'I'|| c == 'O'|| c == 'U') 			System.out.println("Vowel"); 		else 			System.out.println("Consonant"); 	} } }

---
+++ Program_to_check_Vowel_or_Consonant.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class A
+ {
+    public static void main(String[] args)
+ {
+      Scanner s = new Scanner(System.in);
+	     char c = s.next().charAt(0);
+	{
+		if (c == 'a' || c == 'e' || c == 'i' || c== 'o' || c == 'u'|| c == 'A'|| c == 'E'|| c == 'I'|| c == 'O'|| c == 'U')
+			System.out.println("Vowel");
+		else
+			System.out.println("Consonant");
+	}
+}
+}
